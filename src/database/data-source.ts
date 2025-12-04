077de12bd45783a855d4208c7374fcd4e455457b
YammD123 <siamqueen52@gmail.com>
2025-12-04 19:50:59 +0700

    fix: memperbaiki generate salah tempat

--- src/database/data-source.ts
+++ src/database/data-source.ts
@@ -1,12 +1,11 @@
 import { DataSource } from 'typeorm';
 import { config } from 'dotenv';
-import * as path from 'path';
 import { User } from '../entities/user.entity';
 import { Profile } from '../entities/profile.entity';
 
 config();
 
-export const AppDataSource = new DataSource({
+export default new DataSource({
   type: 'mysql',
   host: process.env.DB_HOST,
   port: Number(process.env.DB_PORT),
@@ -14,5 +13,5 @@ export const AppDataSource = new DataSource({
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
   entities: [User, Profile],
-  migrations: [path.join(__dirname, 'migrations/*.ts')], // <── PENTING
+  migrations: ['src/database/migrations/*.ts'],
 });
